36b2bca0b6f35690fadb96342d2992926d896197
Nazrul Islam Khan <nazrulkhan.dk@gmail.com>
2020-12-30 17:38:16 +0600

    User signup added

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Contact
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -8,7 +9,7 @@ def home(request):
 
 
 def contact(request):
-    if request.method=="POST":
+    if request.method == "POST":
         name = request.POST['name']
         email = request.POST['email']
         phone = request.POST['phone']
@@ -24,3 +25,39 @@ def contact(request):
 
 def about(request):
     return render(request, 'home/about.html')
+
+
+def handlesignup(request):
+    # request validation
+    try:
+        if request.method != 'POST':
+            messages.error(request, "Please fill up the form correctly!")
+            return redirect(request.META['HTTP_REFERER'])
+
+
+        #  collect User information
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+            # form validation
+        if password1 != password2:
+            messages.error(request, "Password didn't match! please try again.")
+            return redirect(request.META['HTTP_REFERER'])  
+        if not username.isalnum():
+            messages.error(request, "Username should only contain lowercase letters and numbers without any special charecter.")
+            return redirect(request.META['HTTP_REFERER']) 
+
+            # create user  
+        myuser = User.objects.create_user(username, email, password1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, "Your account has been successfully created")
+        return redirect(request.META['HTTP_REFERER'])
+    except:
+        return render(request, 'home/home.html')
+
